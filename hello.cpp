085a2ab82213b3e017aa4ada9d7ebb1fd162dc31
my_library <1661513755@qq.com>
2020-10-11 09:57:35 +0800

    the

--- hello.cpp
+++ hello.cpp
@@ -11,7 +11,11 @@ int main(int argc,char* argv[])
 	int b = 20;
 	int c = Nultiply(a,b); 
 	std::cout<<"hello C"<<std::endl;
+<<<<<<< Updated upstream
 	std::cout<<"a*b = "<<c<<std::endl;
+=======
+	std::cout<<"b*a = "<<c<<std::endl;
+>>>>>>> Stashed changes
 	return 0;
 }
 
