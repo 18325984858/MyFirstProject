afb247618afca3858ba58f4cf39a7b8ee0a03efa
my_library <1661513755@qq.com>
2020-10-11 09:47:44 +0800

    two update

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 
+int Nultiply(int a,int b)
+{
+	return a*b;
+}
+
 int main(int argc,char* argv[])
 {
+	int a = 10;
+	int b = 20;
+	int c = Nultiply(a,b); 
 	std::cout<<"hello C"<<std::endl;
+	std::cout<<"a*b = "<<c<<std::endl;
 	return 0;
 }
 
